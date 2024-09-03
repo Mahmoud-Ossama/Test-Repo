f629100722387ab8b6f487a2d6b388a2b23e5670
Mahmoud <osamox@proton.me>
2024-09-03 21:46:06 +0300

    connecting the html to app.py

--- app.py
+++ app.py
@@ -16,10 +16,14 @@ x_new = np.array([[0], [2]])
 y_predict = model.predict(x_new)
 
 # Plot the results
+plt.figure(figsize=(10, 5), facecolor='lightgray')  # Set figure size and background color
 plt.scatter(x, y)
 plt.plot(x_new, y_predict, color='red', linewidth=2)
 plt.title('Linear Regression Example')
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.grid()
-plt.show()
+
+# Save the plot as an image
+plt.savefig('linear_regression_plot.png', dpi=300, bbox_inches='tight')
+plt.close()  # Close the plot to free memory
